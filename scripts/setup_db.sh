76fdff2b844693ed8d1bf03f3e9555583b4f525e
Vitor Pellegrino <vitorp@gmail.com>
2015-12-18 20:15:16 +0100

    Waits for mysql to be up before creating the database

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# waits for mysql to boot
+while netstat -lnt | awk '$4 ~ /:3306$/ {exit 1}'; do sleep 5; done
 docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -uroot -proot -e "GRANT ALL PRIVILEGES ON * . * TO msvcdir;"'
 docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -umsvcdir -pmsvcdir -e "create database microservicesdirtest;"'
 docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -umsvcdir -pmsvcdir microservicesdirtest < sql/create_schema.sql'
