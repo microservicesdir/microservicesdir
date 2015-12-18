b7609e97bffcbbcdfaca7d7131ce60c89b824874
Vitor Pellegrino <vitorp@gmail.com>
2015-12-18 20:09:36 +0100

    fixup! Removes additional services from ci

--- scripts/mysql-repl.sh
+++ scripts/mysql-repl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h localhost -umsvcdir -pmsvcdir microservicesdirtest'
+docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -umsvcdir -pmsvcdir microservicesdirtest'
--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -uroot -proot -e "create database microservicesdirtest;"'
-docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -uroot -proot microservicesdirtest < sql/create_schema.sql'
+docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -uroot -proot -e "GRANT ALL PRIVILEGES ON * . * TO msvcdir;"'
+docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -umsvcdir -pmsvcdir -e "create database microservicesdirtest;"'
+docker run -w `pwd` -v `pwd`:`pwd` --net=host -it --rm mysql/mysql-server sh -c 'exec mysql -h 0.0.0.0 -umsvcdir -pmsvcdir microservicesdirtest < sql/create_schema.sql'
